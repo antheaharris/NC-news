7df591f74d8cc697e91653323fff49275db922af
Anthea Harris <antheaharris@Antheas-MacBook-Air.local>
2019-06-24 12:02:22 +0100

    wrote migration scripts & created topics table

--- knexfile.js
+++ knexfile.js
@@ -1,30 +1,23 @@
-const ENV = process.env.NODE_ENV || 'development';
+const ENV = process.env.NODE_ENV || "development";
 
 const baseConfig = {
-  client: 'pg',
-  migrations: {
-    directory: './db/migrations'
-  },
-  seeds: {
-    directory: './db/seeds'
-  }
+  client: "pg",
+  migrations: { directory: "./db/migrations" },
+  seeds: { directory: "./db/seeds" }
 };
 
 const customConfig = {
   development: {
-    connection: {
-      database: 'nc_news'
-      // username,
-      // password
-    }
+    connection: { database: "nc_news" }
   },
   test: {
-    connection: {
-      database: 'nc_news_test'
-      // username,
-      // password
-    }
+    connection: { database: "nc_news_test" }
   }
 };
 
-module.exports = { ...customConfig[ENV], ...baseConfig };
+const dbConfig = {
+  ...baseConfig,
+  ...customConfig[ENV]
+};
+
+module.exports = dbConfig;
