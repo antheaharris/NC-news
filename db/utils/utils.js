c1b3d07edd00adcb351cbb837df3d1b2d2d91fb4
Anthea Harris <antheaharris@Antheas-Air.default>
2019-06-30 18:32:41 +0100

    refactored formatComments util function

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -17,9 +17,14 @@ exports.makeRefObj = list => {
 exports.formatComments = (comments, articleRef) => {
   return comments.map(comment => {
     let { belongs_to, created_by, created_at, ...restOfComment } = comment;
-    const article_id = articleRef[belongs_to];
-    const author = comment.created_by;
-    created_at = new Date(comment.created_at);
-    return { article_id, author, created_at, ...restOfComment };
+    //const article_id = articleRef[belongs_to];
+    //const author = comment.created_by;
+    //created_at = new Date(comment.created_at);
+    return {
+      ...restOfComment,
+      article_id: articleRef[belongs_to],
+      author: comment.created_by,
+      created_at: new Date(comment.created_at)
+    };
   });
 };
