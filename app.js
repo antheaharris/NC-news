cfe57a10f8eb5f464f1335d835797d308703feab
Anthea Harris <antheaharris@Antheas-MacBook-Air.local>
2019-07-15 12:41:39 +0100

    installed express cors and updated app.js to use it

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors')
 const apiRouter = require("./routes/api");
 const app = express();
 const {
@@ -9,6 +10,8 @@ const {
 } = require("./errors");
 
 app.use(express.json());
+
+app.use(cors())
 
 app.use("/api", apiRouter);
 
